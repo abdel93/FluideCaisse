bf2ed9d9916c06be037227e8dfc3bafef4290e38
Abdelrazak Tazir <raz2512@hotmail.com>
2015-02-16 18:11:48 +0100

    changements effectués

---
+++ src/main/java/fluideCorp/fluidecaisse/App.java
@@ -0,0 +1,13 @@
+package fluideCorp.fluidecaisse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
